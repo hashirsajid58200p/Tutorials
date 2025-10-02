9aa634af31d9dd4596537486ee998b227d00d681
Hashir Sajid <hs96300k@gmail.com>
2025-10-02 20:19:42 +0500

    Updated

---
+++ node.js-tutorial-by-piyush-garg/server/index.js
@@ -0,0 +1,13 @@
+const http = require("http");
+const fs = require("fs");
+
+const myServer = http.createServer((req, res) => {
+  const log = `${Date.now()}: ${req.url} New req received\n`;
+  fs.appendFile("log.txt", log, (err, data) => {
+    res.end("Hello from server");
+  });
+});
+
+myServer.listen(8000, () => {
+  console.log("Server is listening on port 8000");
+});

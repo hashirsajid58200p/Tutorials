2828f8a9a9867ed5b075ac996174321429b65add
Hashir Sajid <hs96300k@gmail.com>
2025-10-02 19:02:18 +0500

    Updated

--- node.js-tutorial-by-piyush-garg/helloworld/file.js
+++ node.js-tutorial-by-piyush-garg/helloworld/file.js
@@ -1,21 +1,43 @@
-const { error } = require("console");
+// const { error } = require("console");
+// const fs = require("fs");
+const os = require("os");
+
+console.log(os.cpus().length);
+
+// //Sync
+// // fs.writeFileSync("./test.txt", "Hey! There HS");
+// // const result = fs.readFileSync("./contacts.txt", "utf-8");
+// // console.log(result);
+
+// //Async
+// // fs.writeFile("./test.txt", "Hey! There HS", (error) => {});
+
+// // const result = fs.readFile("./contacts.txt", "utf-8", (error) => {
+// //   if (error) {
+// //     console.log(error);
+// //   } else {
+// //     return result;
+// //   }
+// // });
+// // console.log(result);
+
+// fs.appendFileSync("./test.txt", `Hey There\n`);
+
 const fs = require("fs");
 
-//Sync
-// fs.writeFileSync("./test.txt", "Hey! There HS");
-// const result = fs.readFileSync("./contacts.txt", "utf-8");
+// Blocking
+
+// console.log("1");
+
+// const result = fs.readFileSync("contacts.txt", "utf-8");
 // console.log(result);
 
-//Async
-// fs.writeFile("./test.txt", "Hey! There HS", (error) => {});
+// Non-Blocking
 
-// const result = fs.readFile("./contacts.txt", "utf-8", (error) => {
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     return result;
-//   }
+// fs.readFile("contacts.txt", "utf-8", (error, result) => {
+//   console.log(result);
 // });
-// console.log(result);
 
-fs.appendFileSync("./test.txt", `Hey There\n`);
+// console.log("2");
+
+// Default Thread Pool Size is 4 more depends on the server processor threads if it has 16 then thread pool size can be increased to 16

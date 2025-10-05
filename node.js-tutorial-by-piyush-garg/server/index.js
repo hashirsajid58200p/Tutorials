03c3b4470222929d55124a428df59e6100f9d576
Hashir Sajid <hs96300k@gmail.com>
2025-10-05 16:13:02 +0500

    Updated

--- node.js-tutorial-by-piyush-garg/server/index.js
+++ node.js-tutorial-by-piyush-garg/server/index.js
@@ -2,9 +2,19 @@ const http = require("http");
 const fs = require("fs");
 
 const myServer = http.createServer((req, res) => {
+  if (req.url === "/favicon.ico") return res.end();
   const log = `${Date.now()}: ${req.url} New req received\n`;
   fs.appendFile("log.txt", log, (err, data) => {
-    res.end("Hello from server");
+    switch (req.url) {
+      case "/":
+        res.end("Home Page");
+        break;
+      case "/about":
+        res.end("About Page");
+        break;
+      default:
+        res.end("Not Found");
+    }
   });
 });
 

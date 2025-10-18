6b6a30a1a3de2566d6577286487b376b42bcf0d9
Hashir Sajid <hs58200p@gmail.com>
2025-10-18 22:46:19 +0500

    Updated

--- node.js-tutorial-by-piyush-garg/server/index.js
+++ node.js-tutorial-by-piyush-garg/server/index.js
@@ -1,37 +1,53 @@
-const http = require("http");
-const fs = require("fs");
-const url = require("url");
+// const fs = require("fs");
+// const url = require("url");
+// const http = require("http");
+const express = require("express");
 
-const myServer = http.createServer((req, res) => {
-  if (req.url === "/favicon.ico") return res.end();
-  const log = `${Date.now()}: ${req.method} ${req.url} New req received\n`;
-  const myUrl = url.parse(req.url, true);
-  console.log(myUrl);
-  fs.appendFile("log.txt", log, (err, data) => {
-    switch (myUrl.pathname) {
-      case "/":
-        if (req.method === "GET") res.end("Home Page");
-        break;
-      case "/about":
-        const username = myUrl.query.myname;
-        res.end(`Hi, ${username}`);
-        break;
-      case "/search":
-        const search = myUrl.query.search_query;
-        res.end("Here are your results for " + search);
-        break;
-      case "/signup":
-        if (req.method === "GET") res.end("This is a sign up form");
-        else if (req.method === "POST") {
-          res.end("Success");
-        }
+const app = express();
 
-      default:
-        res.end("Not Found");
-    }
-  });
+app.get("/", (req, res) => {
+  return res.send("Hello from Home Page");
+});
+app.get("/About", (req, res) => {
+  return res.send("Hello from About Page" + " Hey " + req.query.name);
 });
 
-myServer.listen(8000, () => {
+// function myHandler(req, res) {
+//   if (req.url === "/favicon.ico") return res.end();
+//   const log = `${Date.now()}: ${req.method} ${req.url} New req received\n`;
+//   const myUrl = url.parse(req.url, true);
+//   console.log(myUrl);
+//   fs.appendFile("log.txt", log, (err, data) => {
+//     switch (myUrl.pathname) {
+//       case "/":
+//         if (req.method === "GET") res.end("Home Page");
+//         break;
+//       case "/about":
+//         const username = myUrl.query.myname;
+//         res.end(`Hi, ${username}`);
+//         break;
+//       case "/search":
+//         const search = myUrl.query.search_query;
+//         res.end("Here are your results for " + search);
+//         break;
+//       case "/signup":
+//         if (req.method === "GET") res.end("This is a sign up form");
+//         else if (req.method === "POST") {
+//           res.end("Success");
+//         }
+
+//       default:
+//         res.end("Not Found");
+//     }
+//   });
+// }
+
+app.listen(8000, () => {
   console.log("Server is listening on port 8000");
 });
+
+// const myServer = http.createServer(app);
+
+// myServer.listen(8000, () => {
+//   console.log("Server is listening on port 8000");
+// });
